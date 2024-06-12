d5fc4131719a59951d069f4ab2e9d8dea3a6659d
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-06-12 09:58:41 +0530

    FEAT: Added the content for the about us page and modified section layout

--- frontend/src/Components/HomePage/SectionLayout.tsx
+++ frontend/src/Components/HomePage/SectionLayout.tsx
@@ -3,19 +3,26 @@ const SectionLayout: React.FC<{
   title: string;
   content?: string;
   extraComponents?: React.ReactNode;
-  extraClasses?: string;
-}> = ({ title, content, extraComponents, extraClasses }) => {
+  headingClasses?: string;
+  contentClasses?: string;
+}> = ({ title, content, extraComponents, contentClasses, headingClasses }) => {
   return (
     <div className="flex max-w-[90%] flex-col gap-5 xl:max-w-[80%]">
-      <h1 className="text-center text-3xl font-bold lg:text-4xl">{title}</h1>
-      <div className="flex flex-col items-center gap-2 overflow-x-auto overflow-y-hidden">
-        <p
-          className={`text-xl lg:text-2xl ${extraClasses} rounded-xl px-6 py-9`}
-        >
-          {content}
-        </p>
-        {extraComponents}
-      </div>
+      <h1
+        className={`text-center text-3xl font-bold lg:text-4xl ${headingClasses}`}
+      >
+        {title}
+      </h1>
+      {content && (
+        <div className="flex flex-col items-center gap-2 overflow-x-auto overflow-y-hidden">
+          <p
+            className={`text-xl lg:text-2xl ${contentClasses} rounded-xl px-6 py-9`}
+          >
+            {content}
+          </p>
+        </div>
+      )}
+      {extraComponents}
     </div>
   );
 };
--- frontend/src/Layouts/AboutUsLayout.tsx
+++ frontend/src/Layouts/AboutUsLayout.tsx
@@ -1,5 +1,47 @@
 import React from "react";
+import SectionLayout from "../Components/HomePage/SectionLayout";
 const AboutUsLayout: React.FC = () => {
-  return <div>About Us</div>;
+  return (
+    <div className="flex h-full flex-col items-center justify-center">
+      <SectionLayout
+        title="About Us"
+        extraComponents={
+          <p className="rounded-xl px-6 py-9 text-xl lg:text-2xl">
+            Speak & Spell Academy is an independent English language school
+            based in Hisar, Haryana. Established in 2023, although still in its
+            early years, our academy is driven by the extensive expertise and
+            experience of our trainers, spanning over two decades in academic
+            teaching and web content creation. We take pride in our approach of
+            offering practical tools for effective communication, rather than
+            making unrealistic promises.<br></br> Our comprehensive range of
+            courses is meticulously designed to cater to various learning needs
+            and proficiency levels to equip students with the necessary skills
+            and knowledge to succeed in their English language journey. From
+            foundational grammar and vocabulary building to advanced
+            conversation and business communication, our diverse offerings
+            ensure that learners receive a well-rounded education. Additionally,
+            our experienced instructors employ innovative teaching methods and
+            engaging materials to create a dynamic and enriching learning
+            experience.<br></br> When you choose Speak & Spell Academy, you're
+            choosing seasoned professionals dedicated to your success.<br></br>{" "}
+            Join us today and embark on a transformative journey towards English
+            proficiency.
+          </p>
+        }
+      />
+      <SectionLayout
+        title="Faculty Info"
+        extraComponents={
+          <p className="rounded-xl px-6 py-9 text-xl lg:text-2xl">
+            With over 19 years of combined experience in teaching and web
+            content development, I provide comprehensive and effective English
+            training services designed to meet the unique needs of each learner.
+            My love for writing, reading, painting, and travelling enriches my
+            dynamic and engaging teaching approach.
+          </p>
+        }
+      />
+    </div>
+  );
 };
 export default AboutUsLayout;
