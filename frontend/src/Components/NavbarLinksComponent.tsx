1a849859588049f9612fea0420ddf1479f6db16d
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-06-12 11:59:21 +0530

    STYLE: Restructured the navbar, removed redundancy

--- frontend/src/Components/NavbarLinksComponent.tsx
+++ frontend/src/Components/NavbarLinksComponent.tsx
@@ -2,21 +2,23 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 interface NavbarLinksComponentProps {
   title: string;
+  refersTo: string;
+  classes?: string;
 }
 const NavbarLinksComponent: React.FC<NavbarLinksComponentProps> = ({
   title,
+  refersTo,
+  classes,
 }) => {
   return (
-    <>
-      <NavLink
-        to={`/${title.toLowerCase()}`}
-        className={({ isActive }) => {
-          return isActive ? "font-semibold text-CelticBlue" : "";
-        }}
-      >
-        {title}
-      </NavLink>
-    </>
+    <NavLink
+      to={`/${refersTo.toLowerCase()}`}
+      className={({ isActive }) => {
+        return `${classes} ${isActive ? "font-semibold text-Sunglow" : "text-white"}`;
+      }}
+    >
+      {title}
+    </NavLink>
   );
 };
 export default NavbarLinksComponent;
--- frontend/src/Layouts/NavbarLayout.tsx
+++ frontend/src/Layouts/NavbarLayout.tsx
@@ -1,30 +1,24 @@
 import React, { useState } from "react";
 import NavbarLinksComponent from "../Components/NavbarLinksComponent";
-import { Link } from "react-router-dom";
 import hamburgerIcon from "../assets/hamburger.svg";
-import HamburgerLinkComponent from "../Components/HamburgerLinkComponent";
 import closeIcon from "../assets/close.svg";
 const NavbarLayout: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => setIsOpen(!isOpen);
   return (
-    <div className="flex h-[4rem] w-full items-center justify-between border-b border-slate-200 p-2 lg:justify-around">
-      <Link
-        className="text-nowrap p-1 font-Roboto text-2xl font-bold md:text-3xl"
-        to="/"
-      >
-        Speak and Spell Academy
-      </Link>
-      <nav className="hidden gap-4 md:flex">
-        <NavbarLinksComponent title="Courses" />
-        <NavbarLinksComponent title="Blogs" />
+    <div className="flex h-[4rem] w-full items-center justify-between border-b border-slate-200 bg-OxfordBlue p-2 lg:justify-around">
+      <NavbarLinksComponent
+        title="Speak and Spell Academy"
+        refersTo=""
+        classes="text-nowrap p-1 font-Roboto text-xl font-bold sm:text-2xl lg:text-3xl"
+      />
+      <nav className="hidden gap-4 md:flex lg:text-lg">
+        <NavbarLinksComponent title="Courses" refersTo="courses" />
+        <NavbarLinksComponent title="Blogs" refersTo="blogs" />
+        <NavbarLinksComponent title="Faq" refersTo="faq" />
+        <NavbarLinksComponent title="About Us" refersTo="aboutus" />
+        <NavbarLinksComponent title="Contact Us" refersTo="contactus" />
       </nav>
-      <Link
-        className="hidden rounded-full bg-OxfordBlue px-4 py-2 text-white md:block"
-        to="/contactus"
-      >
-        Contact Us
-      </Link>
       <div className="relative md:hidden">
         <img
           src={isOpen ? closeIcon : hamburgerIcon}
@@ -35,12 +29,34 @@ const NavbarLayout: React.FC = () => {
         {isOpen && (
           <div className="flex flex-col">
             <nav
-              className="absolute -right-2 top-[3rem] z-50 flex h-screen w-screen flex-col items-center gap-5 bg-neutral-100/90 p-4"
+              className="absolute -right-2 top-[2.5rem] z-50 flex h-screen w-screen flex-col items-center gap-5 bg-slate-800/90 p-4"
               onClick={toggleMenu}
             >
-              <HamburgerLinkComponent title="Courses" />
-              <HamburgerLinkComponent title="Blogs" />
-              <HamburgerLinkComponent title="Contact Us" />
+              <NavbarLinksComponent
+                title="Courses"
+                refersTo="courses"
+                classes="w-full text-center p-2 rounded-lg underline"
+              />
+              <NavbarLinksComponent
+                title="Blogs"
+                refersTo="blogs"
+                classes="w-full text-center p-2 rounded-lg underline"
+              />
+              <NavbarLinksComponent
+                title="Faq"
+                refersTo="faq"
+                classes="w-full text-center p-2 rounded-lg underline"
+              />
+              <NavbarLinksComponent
+                title="About Us"
+                refersTo="aboutus"
+                classes="w-full text-center p-2 rounded-lg underline"
+              />
+              <NavbarLinksComponent
+                title="Contact Us"
+                refersTo="contactus"
+                classes="w-full text-center p-2 rounded-lg underline"
+              />
             </nav>
           </div>
         )}
