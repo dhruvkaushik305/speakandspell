6a57b7d9d3ab6bce7d520dff3f50f7cfd738b43d
Dhruv Kaushik <dhruvkaushik305@gmail.com>
2024-06-26 11:28:31 +0530

    STYLE: Changed the colors of the banner text to make it readable. Commented out the testimonial

--- frontend/src/Components/HomePage/BannerComponent.tsx
+++ frontend/src/Components/HomePage/BannerComponent.tsx
@@ -2,7 +2,7 @@ import React from "react";
 const BannerComponent: React.FC = () => {
   return (
     <div className="flex h-[30rem] w-full items-end justify-center bg-studentsOnTable bg-cover bg-top">
-      <div className="w-full p-3 text-center font-Roboto text-2xl backdrop-blur-2xl lg:text-3xl xl:text-4xl">
+      <div className="w-full bg-black/45 p-3 text-center font-Roboto text-2xl text-Primary/80 backdrop-blur-2xl lg:text-3xl xl:text-4xl">
         Your transformative journey towards{" "}
         <span className="italic">confident communication</span> begins here.
       </div>
--- frontend/src/Layouts/HomeLayout.tsx
+++ frontend/src/Layouts/HomeLayout.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import WhyChooseUsComponent from "../Components/HomePage/WhyChooseUsComponent";
-import TestimonialsComponent from "../Components/HomePage/TestimonialsComponent";
+// import TestimonialsComponent from "../Components/HomePage/TestimonialsComponent";
 import BannerComponent from "../Components/HomePage/BannerComponent";
 import AboutUsComponent from "../Components/HomePage/AboutUsComponent";
 import WhyEnglishComponent from "../Components/HomePage/WhyEnglishComponent";
 const HomeLayout: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-16">
+    <div className="mb-10 flex flex-col items-center justify-center gap-16">
       <BannerComponent />
       <AboutUsComponent />
       <WhyChooseUsComponent />
       <WhyEnglishComponent />
-      <TestimonialsComponent />
+      {/* <TestimonialsComponent /> */}
     </div>
   );
 };
